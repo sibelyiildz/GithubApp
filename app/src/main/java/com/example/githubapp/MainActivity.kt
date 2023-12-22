8184d870c2dc5806a614c23bf626f1db8179ed08
Sibel Yıldız <sibelyldz2012@gmail.com>
2023-12-22 22:01:09 +0300

    nav graph implementation

--- app/src/main/java/com/example/githubapp/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.githubapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.githubapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
